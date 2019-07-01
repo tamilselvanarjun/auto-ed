e755d98909933e9b75a752ebc654b96a69fc0ce7
lindseysbrown <lindseysbrown@hotmail.com>
2019-07-01 08:04:34 -0400

    use correct version of ADnum for graph

--- ADgraph_GUI.py
+++ ADgraph_GUI.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
 import pandas as pd
-from AD20.ADnum import ADnum
+from ADnum_rev_timed_vis import ADnum
 from mpl_toolkits.mplot3d import Axes3D
 
 def gen_graph(y):
