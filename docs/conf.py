fa1679e9404d958ed173998925238ada0d166f58
Rachel Moon <rachelmoon@Rachels-MacBook-Pro.local>
2020-12-18 09:29:17 -0500

    readthedocs add css

--- docs/conf.py
+++ docs/conf.py
@@ -101,6 +101,9 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+def setup(app):
+  app.add_stylesheet( "css/s4defs-roles.css" )
+
 html_css_files = ['css/s4defs-roles.css']
 
 # -- Options for HTMLHelp output ------------------------------------------
